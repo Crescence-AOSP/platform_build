b94836b6752995b0a1aac2043c0e97a216fa0235
Lunia <lunia0251@proton.me>
2026-09-15 14:25:07 +0700

    build: Exclude AOSP audio and APNs list from CUSTOM_BUILD
    Change-Id: I7afa48285807ac96f3c5f44d36fb4e7e43af3897

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -18,8 +18,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
+ifndef CUSTOM_BUILD
 # Default AOSP sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+endif
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -35,7 +37,9 @@ PRODUCT_PACKAGES += \
     preinstalled-packages-platform-aosp-product.xml \
     ThemePicker \
 
+ifndef CUSTOM_BUILD
 # Telephony:
 #   Provide a APN configuration to GSI product
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
